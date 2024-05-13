ccf2e8e0d53cc51fe24798544ee4140c45ade2c1
ShaikHassain <hassainshaik347@gmail.com>
2024-05-13 20:07:47 +0530

    basicauth

---
+++ src/selenium/basicauth.java
@@ -0,0 +1,18 @@
+package selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class basicauth {
+    public static void main(String []args)
+    {
+        System.setProperty("webdriver.chrome.driver", "C:\\selenium\\chromedriver_win32\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.selenium.dev/");
+        String username = "admin";
+        String password = "admin";
+        String url = "https://"+username+":"+password+"the-internet.herokuapp.com/basic_auth";
+
+    }
+}
